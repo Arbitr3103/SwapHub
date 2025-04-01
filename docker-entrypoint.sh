477f533abd690893336d8e4d68ef8992102717c3
VladimirArbitr <bragin.arbitr@icloud.com>
2025-04-01 14:16:38 +0300

    Add initial data loading to entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,6 +11,12 @@ python manage.py collectstatic --noinput
 echo "Running migrations..."
 python manage.py migrate
 
+# Загрузка начальных данных, если база пустая
+if [ "$(python manage.py dumpdata auth.User --indent 2 | grep '\[]')" ]; then
+    echo "Loading initial data..."
+    python manage.py loaddata db_dump.json
+fi
+
 # Используем PORT из Render или 10000 по умолчанию
 PORT="${PORT:-10000}"
 echo "Starting Gunicorn on port $PORT..."
